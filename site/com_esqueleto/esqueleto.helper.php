40cca23cb8ebf2c12099b0502d9ef1bbf9bd6d53
Carlos Delfino Carvalho Pinheiro <consultoria@carlosdelfino.eti.br>
2012-12-11 20:41:02 -0200

    adicionado comentário no metodo loadTables();

--- site/com_esqueleto/esqueleto.helper.php
+++ site/com_esqueleto/esqueleto.helper.php
@@ -9,6 +9,11 @@ class  EsqueletoHelper {
 	 * Cada metodo deve ser um factory ou um método auxiliar.
 	 */
 	
+	/**
+	 * Informa que se pode encontrar os objetos de tabela no diretorio "tables
+	 * 
+	 * Por padrão é procurado no diretorio "table" no singular.
+	 */
 	public static function loadTables(){
 		JTable::addIncludePath(JPATH_COMPONENT_SITE.DS.'tables');
 	}
